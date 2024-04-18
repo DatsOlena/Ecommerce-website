b98f7aff45cf293f21c213ab6bc1da5c89e3ce02
Olena Dats <olena.dats@screeningeagle.com>
2024-04-18 17:44:11 +0200

    Remove signInWithGoogleRedirect

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,11 +1,15 @@
-import { signInWithGooglePopup, createUserProfileDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+import { 
+    signInWithGooglePopup, 
+    createUserDocumentFromAuth
+} from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
-const logGoogleUser = async () => { 
-    const { user } = await signInWithGooglePopup();
-    const userDocRef = await createUserProfileDocumentFromAuth(user);
-};
 
+    const logGoogleUser = async () => { 
+        const { user } = await signInWithGooglePopup();
+        const userDocRef = await createUserDocumentFromAuth(user);
+    };
+    
     return (
         <div>
             <h1>Sign In</h1>
