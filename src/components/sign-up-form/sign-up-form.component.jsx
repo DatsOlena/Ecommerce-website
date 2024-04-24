fd1a445bbfd4d4d07c006ea6a62a87f3542b5e30
Olena Dats <olena.dats@screeningeagle.com>
2024-04-24 22:01:50 +0200

    Create button component

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './sign-up-form.styles.scss';
 
 import FormInput from '../form-input/form-input.component';
+import Button from '../button/button.component';
 
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
 
@@ -94,7 +95,7 @@ const SignUpForm = () => {
                value={confirmPassword}
             />
 
-                <button type="submit">Sign up</button>
+            < Button type="submit">Sign up</Button>
             </form>
         </div>
     )
