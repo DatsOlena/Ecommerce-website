c3c97131ef0dbfa3212f379e4337016c1c33f41b
DatsOlena <datsolena@gmail.com>
2025-07-28 22:18:45 +0200

    Improve formatting

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,24 +1,22 @@
-import React from "react";
 
-import { useContext } from "react";
+import { useContext } from 'react';
+
+import ProductCard from '../../components/product-card/product-card.component';
+
+import { ProductsContext } from '../../contexts/products.context';
 
 import './shop.styles.scss';
 
-import { ProductsContext } from "../../contexts/products.context";
-import ProductCard from "../../components/product-card/product-card.component";
+const Shop = () => {
+  const { products } = useContext(ProductsContext);
 
-
-const Shop = () => {  
-    const { products } = useContext(ProductsContext);
-
-    return (
+  return (
     <div className='products-container'>
-    {products.map(( product )=>(
-           <ProductCard key={ product.id } product={ product }/>
-        ))}
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
     </div>
-    );
+  );
 };
-
 
 export default Shop;
