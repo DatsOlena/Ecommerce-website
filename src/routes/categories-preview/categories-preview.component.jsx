513184fef9bead94de5c10c82aafafbfd22a3646
DatsOlena <datsolena@gmail.com>
2025-08-02 18:26:06 +0200

    Implement category routing, update app.js

---
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -0,0 +1,26 @@
+
+import { useContext, Fragment } from 'react';
+
+import { CategoriesContext } from '../../contexts/categories.context';
+
+import CategoryPreview from '../../components/category-preview/category-preview.component';
+
+
+
+const CategoriesPreview = () => {
+  const { categories } = useContext(CategoriesContext);
+
+  return (
+    <>
+      {
+        Object.keys(categories).map((title) => {
+          const products = categories[title];
+          return <CategoryPreview key={title} title={title} products={products} />
+        })
+      }
+    </>
+
+  );
+};
+
+export default CategoriesPreview;
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,33 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
 
-import { useContext, Fragment } from 'react';
+import CategoriesPreview from '../categories-preview/categories-preview.component';
+import Category from '../category/category.component';
 
-import ProductCard from '../../components/product-card/product-card.component';
-
-import { CategoriesContext } from '../../contexts/categories.context';
 
 import './shop.styles.scss';
 
 const Shop = () => {
-  const { categories } = useContext(CategoriesContext);
+
 
   return (
-    <Fragment>
-      {
-        Object.keys(categories).map((title) => (
-          <Fragment key={title}>
-            <h2>{title}</h2>
-            <div className='products-container'>
-              {categories[title].map((category) => (
-                <ProductCard key={category.id} product={category} />
-              ))}
-            </div>
-
-          </Fragment>
-        ))
-      }
-
-    </Fragment>
-
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=':category' element={<Category />} />
+    </Routes>
   );
 };
 
