2867d5fd5972b6dfbb0d9cd07498ca111bac1c8e
DatsOlena <datsolena@gmail.com>
2025-08-18 22:37:19 +0200

    Convert styles to css-in-js

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,35 +1,34 @@
-import { useContext, useState, useEffect } from 'react';
+import { useState, useEffect, Fragment } from 'react';
+import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-
-import { selectCategoriesMap } from '../../store/categories/categories.selector';
 
 import ProductCard from '../../components/product-card/product-card.component';
 
-import './category.styles.scss';
-import { useSelector } from 'react-redux';
+import { selectCategoriesMap } from '../../store/categories/categories.selector';
 
+import { CategoryContainer, Title } from './category.styles';
 
 const Category = () => {
-    const { category } = useParams();
-    const categoriesMap = useSelector(selectCategoriesMap);
-    const [products, setProducts] = useState(categoriesMap[category]);
+  const { category } = useParams();
+  const categoriesMap = useSelector(selectCategoriesMap);
+  console.log('rendering category');
+  const [products, setProducts] = useState(categoriesMap[category]);
 
-    useEffect(() => {
-        setProducts(categoriesMap[category]);
-    }, [category, categoriesMap]);
+  useEffect(() => {
+    setProducts(categoriesMap[category]);
+  }, [category, categoriesMap]);
 
-    return (
-        <>
-            <h2 className='title'>{category.toLocaleUpperCase()}</h2>
-            <div className='category-container'>
-                {products &&
-                products.map(product => (
-                    <ProductCard key={product.id} product={product} />
-                ))}
-        </div>
-        </>
-    );
-}
-
+  return (
+    <Fragment>
+      <Title>{category.toUpperCase()}</Title>
+      <CategoryContainer>
+        {products &&
+          products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </CategoryContainer>
+    </Fragment>
+  );
+};
 
 export default Category;
