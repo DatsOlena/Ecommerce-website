96a9254a7464985f0edaa0001c2889dbdffbb6c3
DatsOlena <datsolena@gmail.com>
2025-09-17 18:21:02 +0200

    Fix sore thunk implementation bug

--- src/store/store.js
+++ src/store/store.js
@@ -3,7 +3,6 @@ import { configureStore } from '@reduxjs/toolkit';
 import { createLogger } from 'redux-logger';
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage';
-import thunk from 'redux-thunk';
  
 import { rootReducer } from './root-reducer';
 
@@ -12,7 +11,7 @@ const logger = createLogger({ collapsed: true });
 const persistConfig = {
   key: 'root',
   storage,
-  blacklist: ['user'] // user will not be persisted
+  blacklist: ['user']
 }
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
@@ -20,7 +19,7 @@ export const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => {
     const base = getDefaultMiddleware({ serializableCheck: false });
-    return process.env.NODE_ENV !== 'production' ? base.concat(logger, thunk) : base;
+    return process.env.NODE_ENV !== 'production' ? base.concat(logger) : base;
   },
   devTools: process.env.NODE_ENV !== 'production',
 });
