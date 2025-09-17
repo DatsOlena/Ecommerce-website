e2342596f6a3a9d3ac3da70d3c7ba8aed23f3074
DatsOlena <datsolena@gmail.com>
2025-09-17 18:43:55 +0200

    Replace async logic with redux thunk

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,33 +1,27 @@
-import {useEffect} from 'react';
+import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { useDispatch } from "react-redux";
 
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
 
-import { setCategories } from '../../store/categories/categories.action';
-
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils.jsx';
+import { fetchCategoriesAsync } from '../../store/categories/categories.action';
 
 
 import './shop.styles.scss';
 
 const Shop = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoriesArray = await getCategoriesAndDocuments('categories');
-      dispatch(setCategories(categoriesArray));
-    }
-    getCategoriesMap();
-  }, []);
+    dispatch(fetchCategoriesAsync());
+  }, [dispatch]);
 
   return (
-      <Routes>
-        <Route index element={<CategoriesPreview />} />
-        <Route path=':category' element={<Category />} />
-      </Routes>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=':category' element={<Category />} />
+    </Routes>
   );
 };
 
