bc5155d26ed990cf4fff1713f8db4d6c7f2c0969
DatsOlena <datsolena@gmail.com>
2025-09-19 22:49:04 +0200

    Add spinner

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -4,13 +4,16 @@ import { useParams } from 'react-router-dom';
 
 import ProductCard from '../../components/product-card/product-card.component';
 
-import { selectCategoriesMap } from '../../store/categories/categories.selector';
+import { selectCategoriesMap, selectCategoriesIsLoading } from '../../store/categories/categories.selector';
 
 import { CategoryContainer, Title } from './category.styles';
+
+import Spinner from '../../components/spinner/spinner';
 
 const Category = () => {
   const { category } = useParams();
   const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectCategoriesIsLoading);
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
@@ -20,12 +23,12 @@ const Category = () => {
   return (
     <Fragment>
       <Title>{category.toUpperCase()}</Title>
-      <CategoryContainer>
+      {isLoading ? <Spinner /> : <CategoryContainer>
         {products &&
           products.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
-      </CategoryContainer>
+      </CategoryContainer>}
     </Fragment>
   );
 };
